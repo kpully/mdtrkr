c4bc5f7de5d92bf1e1806206135a8bcc549c6710
Katherine Pully <kpully@gmail.com>
2016-09-11 16:06:49 -0400

    changed manage.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class ProductionConfig(Config):
