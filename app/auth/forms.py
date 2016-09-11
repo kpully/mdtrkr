2809c4262505ad524cdce3b123268e91107cc016
Katherine Pully <kpully@gmail.com>
2016-09-11 17:15:02 -0400

    changes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, SelectField
-from wtforms.validators import Required, Length, Regexp, EqualTo
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
 
